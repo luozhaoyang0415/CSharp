6596bbd9e97af1844ae1cd65ec6b1ca4a9c88061
luozhaoyang0415 <1873997927@qq.com>
2018-10-15 16:11:15 +0800

    new files

---
+++ test2/openXmlElement/openXmlElement/Program.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openXmlElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"F:\工作\github\CSharp\test2\科研细则.docx";
+            using (WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(fileName, false))
+            {
+                // 获取主体
+                Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+
+                // 获取所有段落
+                IEnumerable<Paragraph> paragraphs =body.Elements<Paragraph>();
+                //遍历段落，输出内容
+                foreach (Paragraph paragraph in paragraphs)
+                {
+                    Console.WriteLine(paragraph.InnerText);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
